78b6e1249719408404197c0a6874f5d3966f37fe
Vincent Lanford <vincentlanford@gmail.com>
2014-11-15 21:32:46 -0600

    refactored grid

--- Game.js
+++ Game.js
@@ -3,36 +3,10 @@ var canvas;
 var context;
 var game;
 
-var MAPHEIGHT = 20
-var MAPWIDTH = 35
 
 function start()
 {
     ASSETS = loadAssets("assets.json", init);
-}
-
-function getRandomArray(size)
-{
-    return Array.apply(null, Array(size))
-        .map(function (_, i){return Math.floor(Math.random() * 10);});
-}
-
-function negitive_ones(dimensions) {
-    var array = [];
-
-    for (var i = 0; i < dimensions[0]; ++i) {
-        array.push(dimensions.length == 1 ? -1 : negitive_ones(dimensions.slice(1)));}
-
-    return array;
-}
-
-function setup_grid(){
-    grid = negitive_ones([MAPWIDTH, MAPHEIGHT + 1]) // extra row for base case.
-    for (var i = 1, i < MAPWIDTH; i++){
-        grid[i, MAPHEIGHT] = 11;
-    }
-
-    return grid;
 }
 
 function init()
@@ -40,8 +14,7 @@ function init()
     canvas = document.getElementById("MainCanvas");
     context = canvas.getContext('2d');
     requestAnimationFrame(frame);
-    game.grid = setup_grid()
-    game.top_row = getRandomArray(MAPWIDTH)
+    game.grid = new Grid()
 
 
 }
@@ -55,12 +28,12 @@ function frame()
 
 function update()
 {
-
+    game.grid.update();
 }
 
 function render()
 {
-
+    game.grid.render();
 }
 
 window.onload = start;
---
+++ Grid.js
@@ -0,0 +1,47 @@
+
+var MAPHEIGHT = 20
+var MAPWIDTH = 35
+
+function getRandomArray(size)
+{
+    var array = Array.apply(null, Array(size))
+    return array.map(function (_, i){
+        return Math.floor(Math.random() * 10);
+    });
+}
+
+function negitive_ones(dimensions) {
+    var array = [];
+
+    for (var i = 0; i < dimensions[0]; ++i) {
+        //recursively calls negative_ones with one less dimension and pushes it onto the array
+        array.push(dimensions.length == 1 ? -1 : negitive_ones(dimensions.slice(1)));
+    }
+
+    return array;
+}
+
+function setup_grid(){
+    grid = negitive_ones([MAPWIDTH, MAPHEIGHT + 1]) // extra row for base case.
+    for (var i = 1, i < MAPWIDTH; i++){
+        grid[i, MAPHEIGHT] = 11;
+    }
+
+    return grid;
+}
+
+function Grid()
+{
+    this.g = setup_grid();
+    this.top_row = getRandomArray(MAPWIDTH);
+}
+
+Grid.prototype.render = function()
+{
+
+}
+
+Grid.prototype.update = function()
+{
+
+}
