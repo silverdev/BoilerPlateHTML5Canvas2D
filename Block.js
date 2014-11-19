ea69c9cb567f1ac9abdb335e9c57093869f4883a
Vincent Lanford <vincentlanford@gmail.com>
2014-11-19 17:01:45 -0600

    the rendering code you asked for

--- Block.js
+++ Block.js
@@ -11,6 +11,15 @@ function Block(x, y, value)
 	this.x = x;
 	this.y = y;
 	this.value = value;
+	this.alive = true;
+	this.active = true;
+	this.frameCount = 0;
+	this.frames = 0;
+
+	this.fromX = x;
+	this.fromY = y;
+	this.toX = x;
+	this.toY = y;
 }
 
 Block.prototype.remake = function(x, y, value)
@@ -18,6 +27,39 @@ Block.prototype.remake = function(x, y, value)
 	this.x = x;
 	this.y = y;
 	this.value = value;
+}
+
+Block.prototype.moveTo = function(x, y, frames)
+{
+	this.fromX = this.x;
+	this.fromY = this.y;
+	this.toX = x;
+	this.toY = y;
+	this.frameCount = 0;
+	this.frames = frames;
+}
+
+lerp = function(X1, X2, time)
+{
+	return (X2 - X1) * (time) + X1
+}
+
+Block.prototype.update = function()
+{
+	if(this.frames === 0)
+	{
+		//No animation to do
+		return;
+	}
+	if(this.frameCount >= this.frames)
+	{
+		//Animation finished
+		this.frameCount = 0;
+		this.frames = 0;
+	}
+	this.x = lerp(this.fromX, this.toX, this.frameCount / this.frames);
+	this.y = lerp(this.fromY, this.toY, this.frameCount / this.frames);
+	this.frameCount++;
 }
 
 Block.prototype.render = function()
